b121e049db495c4dd0610f0ce4495508f14a93f7
HELLOIAMAGAMER <Vytisb@vitiho.com>
2025-01-08 16:23:25 -0800

    4352

--- ui/pause_menu.gd
+++ ui/pause_menu.gd
@@ -26,11 +26,9 @@ func _on_resume_pressed() -> void:
 	
 func _on_restart_pressed() -> void:
 	get_tree().reload_current_scene()
-		
-		
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 func _process(delta: float):
+	get_tree().paused = false
 	didgamejuststart()
 	testEsc()
