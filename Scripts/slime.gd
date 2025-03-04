96f6cfeca7cdc729f7979c2dfd17aadf9f132601
HELLOIAMAGAMER <Vytisb@vitiho.com>
2025-03-03 18:02:58 -0800

    asfd

--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -6,10 +6,9 @@ const SPEED=60
 @onready var area = $enemyarea
 
 func _physics_process(delta):
+	position.x += SPEED * delta
 	var slime = $Sprite2D
 	updateanimations(slime)
 func updateanimations(slime):
 	ap.play("idle")
 	
-func _process(delta):
-	position.x += SPEED * delta
