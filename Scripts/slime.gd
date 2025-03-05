3209bfa07f91421ba543cf4f96cbedc7ce0f5f8c
Benjo4 <Brpolesetsky@gmail.com>
2025-03-04 17:52:05 -0800

    Makes slimes go back in forth after hiting wall

--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -2,13 +2,24 @@ extends Node2D
 
 const SPEED=60
 
+var direction = 1
+
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+
+
 @onready var ap =  $AnimationPlayer
 @onready var area = $enemyarea
 
 func _physics_process(delta):
-	position.x += SPEED * delta
+	position.x += direction * SPEED * delta
 	var slime = $Sprite2D
 	updateanimations(slime)
 func updateanimations(slime):
 	ap.play("idle")
 	
+func _process(delta):
+	if ray_cast_right. is_colliding():
+		direction = -1
+	if ray_cast_left. is_colliding():
+		direction = 1
